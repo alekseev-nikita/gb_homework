2009fb94b33f55be1c2c80cd886ec76c2daa8f66
alekseev-nikita <81589585+alekseev-nikita@users.noreply.github.com>
2022-04-07 23:31:35 +0300

    hw_04

---
+++ hw_04/hw_04_05.py
@@ -0,0 +1,6 @@
+from utils import get_cur
+import sys
+
+
+if __name__ == '__main__':
+    print(get_cur(sys.argv[1]))
---
+++ hw_04/utils.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def get_cur(cur='USD'):
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    value = f'{cur} not found'
+    report_date = datetime.strptime(soup.find('valcurs').get('date'), '%d.%m.%Y')
+    for valtag in soup.find_all('valute'):
+        if valtag.find('charcode').text == cur:
+            value = valtag.find('value').text
+    return value, f'{report_date.day} {report_date.strftime("%B")}'
