5d2954c506530ae8c884023a49c45ddfef070257
alekseev-nikita <81589585+alekseev-nikita@users.noreply.github.com>
2022-04-12 15:53:46 +0300

    optimized

--- hw_05/hw_05_05.py
+++ hw_05/hw_05_05.py
@@ -5,5 +5,19 @@
 # result = [23, 1, 3, 10, 4, 11]
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-no_repeats = [i for i in src if src.count(i) == 1]
+no_repeats = set()
+repeats = set()
+
+for num in src:
+    if num in repeats:
+        continue
+    if num in no_repeats:
+        no_repeats.discard(num)
+        repeats.add(num)
+        continue
+    no_repeats.add(num)
+
 print(no_repeats)
+
+result = [num for num in src if num in no_repeats]
+print(result)
