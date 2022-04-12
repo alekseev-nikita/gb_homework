b74c6583a97028e129ae85831a918fa02b59b858
alekseev-nikita <81589585+alekseev-nikita@users.noreply.github.com>
2022-04-12 17:05:58 +0300

    to def

--- hw_05/hw_05_05.py
+++ hw_05/hw_05_05.py
@@ -3,21 +3,20 @@
 # списке, например:
 # src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 # result = [23, 1, 3, 10, 4, 11]
+def get_unique_with_set(lst):
+    no_repeats = set()
+    repeats = set()
+    for num in lst:
+        if num in repeats:
+            continue
+        if num in no_repeats:
+            no_repeats.discard(num)
+            repeats.add(num)
+            continue
+        no_repeats.add(num)
+    return no_repeats
+
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-no_repeats = set()
-repeats = set()
-
-for num in src:
-    if num in repeats:
-        continue
-    if num in no_repeats:
-        no_repeats.discard(num)
-        repeats.add(num)
-        continue
-    no_repeats.add(num)
-
-print(no_repeats)
-
-result = [num for num in src if num in no_repeats]
+result = [num for num in src if num in get_unique_with_set(src)]
 print(result)
